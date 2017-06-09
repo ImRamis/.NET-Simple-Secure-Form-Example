5c5a28efab551cda00e428853cb8287fe63137a0
Muhammad Ramis <mr.iramis@hotmail.com>
2017-06-10 03:45:09 +0500

    .NET Demonstration of a Simple Secured Login

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace pw_work
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.ForeColor = System.Drawing.Color.Lime;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(456, 87);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(97, 175);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(109, 228);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(97, 202);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(116, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "DEMO Login";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.InfoText;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.ForeColor = System.Drawing.Color.Lime;
+            this.textBox4.Location = new System.Drawing.Point(264, 105);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(213, 173);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.Text = resources.GetString("textBox4.Text");
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(210, 39);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "DEMO Data use this put valid and invalid : \r\nusername : theU6neal\r\npassword : obj" +
+    "k43n$!@";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 532);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "IP : ";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(15, 309);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(229, 171);
+            this.textBox5.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 293);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "LOGS : ";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(250, 309);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(229, 171);
+            this.textBox6.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(250, 290);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(137, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "how its stored actually in db";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(30, 234);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(78, 17);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Original PC";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(489, 554);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿#region
+
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+#endregion
+
+namespace pw_work
+{
+    public partial class Form1 : Form
+    {
+        private static readonly Random R = new Random();
+        private readonly UnicodeEncoding _byteConverter = new UnicodeEncoding();
+
+        private readonly string _ip = Dns.GetHostEntry("localhost")
+            .AddressList
+            .FirstOrDefault(t => t.AddressFamily == AddressFamily.InterNetwork)
+            ?.ToString();
+        private readonly string _pw;
+        private readonly string _un;
+
+
+        private int _logintimes;
+        private SmtpClient _mailClient;
+
+        private RSAParameters _rsaParams = new RSAParameters
+        {
+            Modulus = new byte[]
+            {
+                22, 126, 210, 83, 160, 73, 40, 39, 201, 155, 19, 202, 3, 11, 191, 178, 56,
+                74, 20, 36, 248, 103, 18, 144, 170, 163, 145, 87, 54, 61, 34, 220, 222,
+                207, 137, 139, 173, 14, 92, 120, 206, 222, 158, 28, 40, 24, 30, 16, 175,
+                108, 128, 35, 230, 118, 40, 121, 113, 125, 216, 130, 11, 24, 90, 48, 194,
+                240, 105, 44, 76, 34, 57, 249, 228, 125, 80, 38, 9, 136, 29, 117, 207, 139,
+                168, 181, 85, 137, 126, 10, 126, 242, 120, 247, 121, 8, 100, 12, 201, 171,
+                38, 226, 193, 180, 190, 117, 177, 87, 143, 242, 213, 11, 44, 180, 113, 93,
+                106, 99, 179, 68, 175, 211, 164, 116, 64, 148, 226, 254, 172, 147
+            }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            label5.Text = @"IP : " + _ip;
+            _un = "theU6neal";
+            _pw = "objk43n$!@";
+            textBox6.Text = @"Username" + Environment.NewLine + RsaEncrypt(_un) + Environment.NewLine +
+                            Environment.NewLine;
+            textBox6.Text += @"Password" + Environment.NewLine + RsaEncrypt(_pw) + Environment.NewLine;
+        }
+
+        public string CodeGen(string n = "")
+        {
+            return n.Length >= 5 ? n : CodeGen(n + (char) R.Next(65, 90));
+        }
+
+        private void SendMail(string code)
+        {
+            _mailClient = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential("mailrandom16@gmail.com", "cryptography"),
+                EnableSsl = true
+            };
+            var message =
+                new MailMessage("mailrandom16@gmail.com", "mailrandom16@gmail.com")
+                {
+                    Subject = "Verify your login from unknown location",
+                    Body = @"Your Verification Code is " + code
+                };
+            try
+            {
+                _mailClient.Send(message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == _un && textBox3.Text == _pw)
+            {
+                if (checkBox1.Checked)
+                {
+                    MessageBox.Show(@"Login Successfull!");
+                    textBox5.Text += @"Login Successfull from IP : " + _ip + Environment.NewLine +
+                                     @"SMS : You Logged in at example" + Environment.NewLine;
+                }
+                else
+                {
+                    if (MessageBox.Show(@"Verify by SMS ? Yes and No for Email ", @"Verification Method",
+                            MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        textBox5.Text += @"SMS : Your Verification Code is '58AB'" + Environment.NewLine;
+                        while (Prompt.ShowDialog("Enter SMS Code", "Verify") != "58AB") ;
+                        textBox5.Text += @"Login Successfull from IP : " + _ip + Environment.NewLine +
+                                         @"SMS : You Logged in at example" + Environment.NewLine;
+                        MessageBox.Show(@"Login Successfull!");
+                    }
+                    else
+                    {
+                        var verificationcode = CodeGen();
+                        textBox5.Text += @"EMAIL Sent Check your email for verification code;" + Environment.NewLine;
+                        SendMail(verificationcode);
+                        while (Prompt.ShowDialog("Enter EMAIL Code", "Verify") != verificationcode) ;
+                        textBox5.Text += @"Login Successfull from IP : " + _ip + Environment.NewLine +
+                                         @"SMS : You Logged in at example" + Environment.NewLine;
+                        MessageBox.Show(@"Login Successfull!");
+                    }
+                }
+            }
+            else
+            {
+                if (_logintimes++ < 3)
+                    textBox5.Text += @"Login Failed from IP : " + _ip + Environment.NewLine + @"Email Notified " +
+                                     Environment.NewLine + @"SMS : Suspicious Activity" + Environment.NewLine;
+                else
+                    textBox5.Text += @"Account Temporary Locked!" + Environment.NewLine;
+                ;
+            }
+        }
+
+
+        private string RsaEncrypt(string value)
+        {
+            var plaintext = Encoding.Unicode.GetBytes(value);
+
+            var cspParams = new CspParameters();
+            cspParams.KeyContainerName = _byteConverter.GetString(_rsaParams.Modulus);
+            using (var rsa = new RSACryptoServiceProvider(2048, cspParams))
+            {
+                var encryptedData = rsa.Encrypt(plaintext, false);
+                return Convert.ToBase64String(encryptedData);
+            }
+        }
+        private string RsaDecrypt(string value)
+        {
+            var encryptedData = Convert.FromBase64String(value);
+            var cspParams = new CspParameters {KeyContainerName = _byteConverter.GetString(_rsaParams.Modulus)};
+            using (var rsa = new RSACryptoServiceProvider(2048, cspParams))
+            {
+                var decryptedData = rsa.Decrypt(encryptedData, false);
+                return Encoding.Unicode.GetString(decryptedData);
+            }
+        }
+    }
+}
